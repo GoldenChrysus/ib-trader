061b8386cc2f916d1624e5bd5cb1f384c93e6c90
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2022-10-22 18:21:29 +0900

    fix(waiter): move code out of loop and consider weekends

--- ib/waiters/time.py
+++ ib/waiters/time.py
@@ -1,14 +1,22 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil import parser
 from pytz import timezone
 from time import sleep
 
 
-def wait_until_time(time: str, tz: str) -> None:
+def wait_until_time(time: str, tz: str, include_weekends=False) -> None:
+    tz_obj = timezone(tz)
+    test = tz_obj.localize(parser.parse(time))
+
+    if not include_weekends:
+        test_weekday = test.weekday()
+
+        if test_weekday in {5, 6}:
+            days_to_add = 7 - test_weekday
+            test = test + timedelta(days=days_to_add)
+
     while True:
-        tz_obj = timezone(tz)
         now = datetime.now(tz_obj)
-        test = tz_obj.localize(parser.parse(time))
 
         if now >= test:
             return
